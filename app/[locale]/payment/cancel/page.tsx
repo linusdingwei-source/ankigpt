945e4512bf0f73a68fcc5d2de4cddc45835a684e
linus.dw <linus.dw@alibaba-inc.com>
2026-01-04 11:21:33 +0800

    修复 ESLint 错误：移除未使用的 usePathname 导入

--- app/[locale]/payment/cancel/page.tsx
+++ app/[locale]/payment/cancel/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { usePathname, Link } from '@/i18n/routing';
+import { Link } from '@/i18n/routing';
 
 export default function PaymentCancelPage() {
   const t = useTranslations();
